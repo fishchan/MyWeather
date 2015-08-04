dff677311a09239e4ac24c6ac5b081d4c11774a1
lilychan <fish19891229@163.com>
2015-08-05 01:01:43 +0800

    增加后台自动更新天气的功能。

---
+++ src/com/myweather/app/AutoUpdateReceiver.java
@@ -0,0 +1,15 @@
+package com.myweather.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
