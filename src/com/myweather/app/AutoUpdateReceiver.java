80853215139b70071d523f7637a8bed6febd1021
lilychan <fish19891229@163.com>
2015-08-05 01:19:42 +0800

    修改程序图标和名称。

--- src/com/myweather/app/AutoUpdateReceiver.java
+++ src/com/myweather/app/AutoUpdateReceiver.java
@@ -9,7 +9,8 @@ public class AutoUpdateReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
-
+		 Intent i = new Intent(context, AutoUpdateService.class);
+         context.startService(i);
 	}
 
 }
