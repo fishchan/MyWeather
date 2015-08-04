f58ae2ea077958c72245feceb3f3e145b2cfe697
lilychan <fish19891229@163.com>
2015-08-04 22:33:17 +0800

    完成遍历省市县三级列表的功能。

--- src/com/myweather/app/ChooseAreaActivity.java
+++ src/com/myweather/app/ChooseAreaActivity.java
@@ -95,6 +95,7 @@ public class ChooseAreaActivity extends Activity {
 	            } else {
 	                queryFromServer(null, "province");
 	        } }
+	        
 	        /**
 	        * 查询选中省内所有的市,优先从数据库查询,如果没有查询到再去服务器上查询。 */
 	        private void queryCities() {
